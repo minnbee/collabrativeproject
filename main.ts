499193a2e7928d3156217c61c1bcad13e0da4459
HarryWhoDKnee <98324751+HarryWhoDKnee@users.noreply.github.com>
2022-01-24 08:28:49 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setVelocity(0, -90)
 })
+let projectile: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(9)
 mySprite = sprites.create(img`
@@ -25,5 +26,22 @@ mySprite.setStayInScreen(true)
 mySprite.setPosition(80, 60)
 mySprite.ay = 200
 game.onUpdateInterval(500, function () {
-	
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . 5 5 5 . . . . . 
+        . . . . . 5 5 5 . . . 5 5 5 5 . 
+        . . . . . . . . . . . . . 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 50, 50)
 })
